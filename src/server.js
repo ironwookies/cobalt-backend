5d3e7c1b00b85a33078748e899956bfca44cead9
Eduardo Gonzalez <eduardgp@gmail.com>
2019-07-31 10:57:59 -0400

    socket io setup

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
-const database = require('./database');
+const socketIo = require('socket.io');
+const axios = require('axios');
+
 const app = require('./app');
 const port = process.env.PORT || 4000;
+
+const database = require('./database');
+
 database.connect();
 const server = app.listen(port, () => {
 	console.log('Express server successfully started...');
 	console.log(`Sever IP address: http://${process.env.IP}:${port}`);
 	console.log(`Express server listening on port ${port}`);
+});
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+	console.log('New client connected'),
+	// setInterval(() => getApiAndEmit(socket), 10000);
+	socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 module.exports = server;
