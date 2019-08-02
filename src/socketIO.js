8364abdd858e80ea8cf9140e4b1fcde9c1eed0a5
Eduardo Gonzalez <eduardgp@gmail.com>
2019-08-02 11:33:01 -0400

    saving the sent message to the database

--- src/socketIO.js
+++ src/socketIO.js
@@ -1,29 +1,30 @@
 const socketIo = require('socket.io');
 const server = require('./server');
 
-const Message = require('./../models/User');
+// const Message = require('./../models/User');
 
 const io = socketIo(server);
 
 const connectEvents = (socket) => {
 	socket.join(socket.handshake['query']._id);
-	console.log('user joined room #' + socket.handshake['query']._id);
+	// console.log('user joined room #' + socket.handshake['query']._id);
 	// socket.on('SEND_MESSAGE', function(data) {
 	// 	io.emit('RECEIVE_MESSAGE', data);
 	// });
 	socket.on('disconnect', function() {
 		socket.leave(socket.handshake['query']._id);
-		console.log('user disconnected from room ', socket.handshake['query']._id);
+		// console.log('user disconnected from room ', socket.handshake['query']._id);
 	});
 
 	socket.on('chat message', function(msg) {
-		console.log(msg);
+		// console.log(msg);
 		io.to(socket.handshake['query']._id).emit('chat message', msg);
 	});
 
 	// socket.on('disconnect', function() {
 	// 	console.log('user disconnected');
 	// });
+
 	socket.on('chat message', function(msg) {
 		console.log('message: ' + msg);
 		socket.broadcast.emit('received', { message: msg });
